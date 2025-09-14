7ce12f94532aaa8c87b5a667207a846ccd38ad51
Thedosia003 <144932326+problemme@users.noreply.github.com>
2025-09-14 20:57:59 +0800

    Update tag_scraper.py

--- tag_scraper.py
+++ tag_scraper.py
@@ -1,22 +1,21 @@
 from bs4 import BeautifulSoup
 from item_scraper import AO3Scraper
 
+ao3_scraper = AO3Scraper()
 class TagScraper():
     def __init__(self):
-        scraper = AO3Scraper()
         self.tag_text_list = []
         self.tag_result_list = []
-        for result in scraper.results:
+        for result in ao3_scraper.results:
             for tag in result["tags"]:
                 self.tag_text_list.append(tag["text"])
     
     def get_tag_list(self):
-        scraper = AO3Scraper()
         for tag_name in self.tag_text_list:
             result = []
-            result = scraper.html_conn(tag_name)
+            result = ao3_scraper.html_conn(tag_name)
             self.tag_result_list.append(result)
         return True
 if __name__ =="main":
-    scraper = TagScraper()
-    scraper.get_tag_list()
+    tag_scraper = TagScraper()
+    tag_scraper.get_tag_list()
