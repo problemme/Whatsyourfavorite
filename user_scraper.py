a5aa963343e7e3df4a1a6679a98e5a48ffa3bb7f
Thedosia003 <144932326+problemme@users.noreply.github.com>
2025-09-14 19:41:19 +0800

    Update user_scraper.py

--- user_scraper.py
+++ user_scraper.py
@@ -1,18 +1,22 @@
 from bs4 import BeautifulSoup
 from item_scraper import AO3Scraper
+import requests
 
+scraper = AO3Scraper()
 # 抓取时选择距离li标签最近的那个div
 class AuthorScraper:
     def __init__(self):
-        scraper = AO3Scraper()
         self.results = scraper.results
         self.work_list = {}
         self.author_url_list = [item["author"]["url"] for item in self.results]
 
     def get_work_list(self):
-        scraper = AO3Scraper()
-        for html in self.author_url_list:
-            soup = BeautifulSoup(html, "lxml")
+        for url in self.author_url_list:
+            headers = {"User-Agent": "Chrome",
+                       "Cookie": f"_otwarchive_session=eyJfcmFpbHMiOnsibWVzc2FnZSI6ImV5SnpaWE56YVc5dVgybGtJam9pTXpFek5tWmxaakZrTm1GbU9ERTNabUV4TW1aaVpqZzRPVEJqTTJVeE56RWlMQ0ozWVhKa1pXNHVkWE5sY2k1MWMyVnlMbXRsZVNJNlcxc3lNelV6TmpneE0xMHNJaVF5WVNReE5DUkJaekYxUlRKTE1HbHpVVUUxZGxKTVEyMXhPRGRsSWwwc0luSmxkSFZ5Ymw5MGJ5STZJaTkxYzJWeWN5OVVhR1ZrYjNOcFlTSXNJbDlqYzNKbVgzUnZhMlZ1SWpvaWVuUk5iRGw2TlY5YWJESXpOWFI1YmtWcmExazRhM0EzYjBwalVteFJSV2xKTXpVeE5GQmhYMGxLY3lKOSIsImV4cCI6IjIwMjUtMDgtMjNUMDU6MzM6MzguMzgxWiIsInB1ciI6ImNvb2tpZS5fb3R3YXJjaGl2ZV9zZXNzaW9uIn19--2d22e0e5ce95447bc994873910afb3f652597617"}
+            responce = requests.get(url, headers = headers, timeout = 60)
+            responce.raise_for_status()
+            soup = BeautifulSoup(responce.text, "lxml")
             work_items = soup.select("li.work.blurb.group")
             for item in work_items:
                 div_header = item.find("div", class_="header module")
